f489eab2a3045bfb04e3f4068ca3a7ab755bb7ed
Barry Devine <barrydevine@Barry-Devines-MacBook-Pro.local>
2026-09-21 15:24:29 +0700

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -24,6 +25,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="font-body antialiased">{children}</body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T1SKR7R0CE"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T1SKR7R0CE');
+        `}
+      </Script>
     </html>
   );
 }
